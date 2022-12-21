605d2fb4891d8cd7a3ff7c0920e09603427305d5
binaya-batas <batas.binaya@gmail.com>
2022-12-21 22:45:26 +0545

    Assignment-1 HTML tags completed.

---
+++ main.js
@@ -0,0 +1,16 @@
+let projectList = document.querySelector('.project-list');
+
+
+var projects = [
+    {
+        'title': 'Day-3 HTML',
+        'code': '',
+        'demo': ''
+    },
+    {}
+]
+
+for (let i=0; i<projects.length; i++) {
+    projectList.innerHTML += 'Hello';
+}
+
