d28b92715934ce9d45fb37c22d320da1ec84fb4a
binaya-batas <batas.binaya@gmail.com>
2023-01-24 22:08:16 +0545

    digital-clock finished but  some issues when displaying stopwatch timer.

--- Javascript/Assessment-2/digital-clock.js
+++ Javascript/Assessment-2/digital-clock.js
@@ -1,3 +1,13 @@
+let ms = 0;
+let sec = 0;
+let min = 0;
+let hrs = 0;
+let int = null;
+
+window.onload = () => {
+    buttonTime.click();
+}
+
 let clock = document.createElement('div');
 clock.setAttribute('class', 'clock')
 
@@ -11,10 +21,29 @@ clock.appendChild(clockOptions);
 let buttonStopwatch = document.createElement('button');
 buttonStopwatch.setAttribute('class', 'clock__options__button clock__options__button--stopwatch');
 buttonStopwatch.innerText = "Stopwatch";
+buttonStopwatch.addEventListener('click', () => {
+    for (let i=0; i<clockFunctionButtons.length; i++) {
+        clockUiTime.classList.remove("show");
+        clockUiTime.classList.add("hidden");
+        clockUiStopwatch.classList.remove("hidden");
+        clockUiStopwatch.classList.add("show");
+        clockFunctionButtons[i].disabled = false;
+    }
+})
 
 let buttonTime = document.createElement('button');
 buttonTime.setAttribute('class', 'clock__options__button clock__options__button--time');
 buttonTime.innerText = "Clock";
+buttonTime.addEventListener('click', () => {
+    console.log("hi")
+    clockUiStopwatch.classList.remove("show");
+    clockUiStopwatch.classList.add("hidden");
+    clockUiTime.classList.remove("hidden");
+    clockUiTime.classList.add("show");
+    for (let i=0; i<clockFunctionButtons.length; i++) {
+        clockFunctionButtons[i].disabled = true;
+    }
+})
 
 clockOptions.appendChild(buttonStopwatch);
 clockOptions.appendChild(buttonTime);
@@ -25,6 +54,31 @@ clock.appendChild(clockUi);
 
 let clockUiStopwatch = document.createElement('div');
 clockUiStopwatch.setAttribute('class', 'clock__ui__stopwatch');
+
+let hours = document.createElement('div')
+hours.setAttribute('class', 'hours');
+hours.innerText = hrs;
+
+let minutes = document.createElement('div');
+minutes.setAttribute('class', 'minutes');
+minutes.innerText = min;
+
+
+let seconds = document.createElement('div');
+seconds.setAttribute('class', 'seconds');
+seconds.innerText = sec;
+
+
+let millSeconds = document.createElement('div');
+millSeconds.setAttribute('class', 'millSeconds');
+millSeconds.innerText = ms;
+
+
+clockUiStopwatch.appendChild(hours);
+clockUiStopwatch.appendChild(minutes);
+clockUiStopwatch.appendChild(seconds);
+clockUiStopwatch.appendChild(millSeconds);
+
 
 let clockUiTime = document.createElement('div');
 clockUiTime.setAttribute('class', 'clock__ui__time');
@@ -42,21 +96,69 @@ let clockFunctions = document.createElement('div');
 clockFunctions.setAttribute('class', 'clock__functions');
 clock.appendChild(clockFunctions);
 
+let clockFunctionButtons = document.getElementsByClassName('clock__functions__button');
+console.log(clockFunctionButtons)
+
 let buttonStart = document.createElement('button');
 buttonStart.setAttribute('class', 'clock__functions__button clock__functions--start');
 buttonStart.innerText = "Start";
+buttonStart.addEventListener('click', () => {
+    console.log(1)
+    if(int !== null){
+        clearInterval(int);
+    }
+    int = setInterval(startTimer,10);
+})
 
 let buttonStop = document.createElement('button');
 buttonStop.setAttribute('class', 'clock__functions__button clock__functions--stop');
 buttonStop.innerText = "Stop";
+buttonStop.addEventListener('click', () => {
+    clearInterval(int);
+})
 
 let buttonReset = document.createElement('button');
 buttonReset.setAttribute('class', 'clock__functions__button clock__functions--reset');
 buttonReset.innerText = "Reset";
+buttonReset.addEventListener('click', () => {
+    hours.innerText = 00;
+    minutes.innerText = 00;
+    seconds.innerText = 00;
+    millSeconds.innerText = 00;
+})
 
 clockFunctions.appendChild(buttonStart);
 clockFunctions.appendChild(buttonStop);
 clockFunctions.appendChild(buttonReset);
+
+
+function startTimer() {
+    ms+=10;
+
+
+    if(ms == 100){
+        ms = 0;
+        sec++;
+
+        console.log("sec", sec)
+
+        if(sec == 60){
+            sec = 0;
+            min++;
+
+            console.log("min", min)
+
+            if(min == 60){
+                min = 0;
+                hrs++;
+
+                console.log("hrs", hrs)
+            }
+        }
+    }
+}
+
+
 
 
 
