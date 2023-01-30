a73c55ce78409d163729db0d9338d61d13b7040e
binaya-batas <batas.binaya@gmail.com>
2023-01-30 08:36:52 +0545

    ant image added.

--- Javascript/Assessment-5/js/script.js
+++ Javascript/Assessment-5/js/script.js
@@ -4,7 +4,6 @@ const BOX_WIDTH = 100;
 const VELOCITY_X = 5;
 const VELOCITY_Y = 2;
 
-
 const particlesArray = [];
 
 const canvas = document.getElementById("canvas");
@@ -18,10 +17,10 @@ class Particle {
     this.x = props.x;
     this.y = props.y;
     (this.vx = VELOCITY_X),
-    (this.vy = VELOCITY_Y),
-    (this.height = BOX_HEIGHT),
-    (this.width = BOX_WIDTH),
-    (this.color = "blue");
+      (this.vy = VELOCITY_Y),
+      (this.height = BOX_HEIGHT),
+      (this.width = BOX_WIDTH),
+      (this.color = "blue");
   }
 
   update() {
@@ -33,8 +32,9 @@ class Particle {
     ctx.fillStyle = this.color;
     ctx.beginPath();
     ctx.fillRect(this.x, this.y, this.height, this.width);
-    ctx.closePath();
-    ctx.fill();
+    let image = new Image();
+    image.src = "assets/ant-image.webp";
+    ctx.drawImage(image, this.x, this.y, this.width, this.height);
   }
 
   checkWallCollision() {
@@ -44,10 +44,7 @@ class Particle {
     ) {
       this.vy = -this.vy;
     }
-    if (
-      this.x + this.vx > canvas.width - this.height ||
-      this.x + this.vx < 0
-    ) {
+    if (this.x + this.vx > canvas.width - this.height || this.x + this.vx < 0) {
       this.vx = -this.vx;
     }
   }
@@ -58,7 +55,7 @@ canvas.addEventListener("click", destroyParticle);
 
 /**
  * Destroys particles when clicked
- * @param {*} event 
+ * @param {*} event
  */
 function destroyParticle(event) {
   let x = event.clientX - canvas.offsetLeft;
@@ -72,7 +69,7 @@ function destroyParticle(event) {
       y <= particle.y + particle.height
     ) {
       cancelAnimationFrame(animationId);
-      let sound = new Audio('assets/smash.mp3');
+      let sound = new Audio("assets/smash.mp3");
       sound.play();
       ctx.clearRect(particle.x, particle.y, particle.width, particle.height);
     }
@@ -86,20 +83,20 @@ function init() {
   for (let i = 0; i < NUMBER_OF_BOXES; i++) {
     let particleX, particleY;
     let overlapping = true;
-    
+
     //checks whether there are any overlaps while generating the boxes.
     while (overlapping) {
       particleX = Math.floor(Math.random() * (canvas.width - BOX_WIDTH));
       particleY = Math.floor(Math.random() * (canvas.height - BOX_HEIGHT));
       overlapping = false;
-    
+
       for (let j = 0; j < particlesArray.length; j++) {
         const otherParticle = particlesArray[j];
         const distance = Math.sqrt(
           (particleX - otherParticle.x) ** 2 +
-          (particleY - otherParticle.y) ** 2
+            (particleY - otherParticle.y) ** 2
         );
-      
+
         if (distance < BOX_WIDTH) {
           overlapping = true;
           break;
@@ -107,7 +104,7 @@ function init() {
       }
     }
 
-    particlesArray.push(new Particle({x: particleX, y: particleY}));
+    particlesArray.push(new Particle({ x: particleX, y: particleY }));
   }
 }
 console.log(particlesArray);
@@ -137,20 +134,21 @@ function animate() {
  * checks collision between the particles(boxes)
  */
 function checkParticleCollision() {
-    for (let i=0; i<particlesArray.length; i++) {
-        for (let j=i+1; j<particlesArray.length; j++) {
-            if(particlesArray[j].x <= particlesArray[i].x + particlesArray[i].width &&
-            particlesArray[j].x + particlesArray[j].width > particlesArray[i].x &&
-            particlesArray[j].y <= particlesArray[i].y + particlesArray[i].height &&
-            particlesArray[j].height + particlesArray[j].y >= particlesArray[j].y)
-            {
-                // particlesArray[j].vy *= -1;
-                particlesArray[j].vx *= -1;
-                particlesArray[i].vx *= -1;
-                // particlesArray[i].vy *= -1;
-            }
-        }
+  for (let i = 0; i < particlesArray.length; i++) {
+    for (let j = i + 1; j < particlesArray.length; j++) {
+      if (
+        particlesArray[j].x <= particlesArray[i].x + particlesArray[i].width &&
+        particlesArray[j].x + particlesArray[j].width > particlesArray[i].x &&
+        particlesArray[j].y <= particlesArray[i].y + particlesArray[i].height &&
+        particlesArray[j].height + particlesArray[j].y >= particlesArray[j].y
+      ) {
+        particlesArray[j].vy *= -1;
+        particlesArray[j].vx *= -1;
+        particlesArray[i].vx *= -1;
+        particlesArray[i].vy *= -1;
+      }
     }
+  }
 }
 
 init();
