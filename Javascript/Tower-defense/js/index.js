c6b72b0c29f9c171be7fc6b783049a4640d2073c
binaya-batas <batas.binaya@gmail.com>
2023-02-06 00:46:48 +0545

    Pathfinding for enemy completed.

--- Javascript/Tower-defense/js/index.js
+++ Javascript/Tower-defense/js/index.js
@@ -1,8 +1,8 @@
 const CANVAS_HEIGHT = window.innerHeight;
 const CANVAS_WIDTH = window.innerWidth;
 
-const canvas = document.querySelector('#canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
 canvas.width = CANVAS_WIDTH;
 canvas.height = CANVAS_HEIGHT;
 
@@ -12,8 +12,55 @@ ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 //background image for canvas.
 const gameMapImage = new Image();
 gameMapImage.onload = () => {
-    ctx.drawImage(gameMapImage, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-}
+  ctx.drawImage(gameMapImage, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+};
 gameMapImage.src = "assets/game-map.png";
 
+class Enemy {
+  constructor(position = {x: 0, y: 0}) {
+    this.position = position;
+    this.width = 100;
+    this.height = 100;
+    this.center = {
+        x: this.position.x + this.width / 2,
+        y: this.position.y + this.height / 2
+      };
+    this.waypointIndex = 0;
+  }
 
+  draw() {
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  debugger;
+  update() {
+    this.draw();
+
+    const waypoint = waypoints[this.waypointIndex];
+    const yDistance = waypoint.y - this.position.y;
+    const xDistance = waypoint.x - this.position.x;
+    const angle = Math.atan2(yDistance, xDistance);
+    this.position.x += Math.cos(angle);
+    this.position.y += Math.sin(angle);
+
+    if (
+      Math.round(this.position.x) === Math.round(waypoint.x) &&
+      Math.round(this.position.y) === Math.round(waypoint.y) &&
+      this.waypointIndex < waypoints.length - 1
+    ) {
+      this.waypointIndex++;
+    }
+  }
+}
+
+const enemy = new Enemy(position = { x: waypoints[0].x, y: waypoints[0].y});
+
+function animate() {
+    requestAnimationFrame(animate);
+    // ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    ctx.drawImage(gameMapImage, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    enemy.update();
+}
+
+animate();
