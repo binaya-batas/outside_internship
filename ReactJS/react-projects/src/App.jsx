bd79eb2df046c256da6b0786d8c72954c4fcc761
binaya-batas <batas.binaya@gmail.com>
2023-02-25 10:29:37 +0545

    assignment-2 final separated.

--- ReactJS/react-projects/src/App.jsx
+++ ReactJS/react-projects/src/App.jsx
@@ -1,37 +1,18 @@
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route, Router } from "react-router-dom";
 
-
-// import Sidebar from './assignment/class/Sidebar/Sidebar'
-// import Ticket from './assignment/class/Tickets/Ticket'
-
-// import Sidebar from './assignment-4/class/Sidebar/Sidebar'
-// import Ticket from './assignment-4/class/Tickets/Ticket'
-
-// import Sidebar from './assignment-5/class/Sidebar/Sidebar'
-// import Ticket from './assignment-5/class/Tickets/Ticket'
-
-import Sidebar from './assignment-6/class/Sidebar/Sidebar'
-import Ticket from './assignment-6/class/Tickets/Ticket'
-import Overview from './assignment-6/class/Overview/Overview';
-
-import Hover from './assignment-5/class/Hover/Hover';
-
-import Login from './assignment-6/class/Login/Login';
-import Signup from './assignment-6/class/SignUp/Signup';
-import SingleTicket from './assignment-6/class/Tickets/SingleTicket/SingleTicket';
+import Sidebar from './assignment-2/class/Sidebar/Sidebar'
+import Ticket from './assignment-2/class/Tickets/Ticket'
 
 import './app.scss';
 import './variables.scss';
-import ErrorPage from './assignment-6/class/Error/ErrorPage';
-import { ToastContainer } from 'react-toastify';
 
 
 function App() {
 
   return (
       <div className="App" style={{ display: 'flex', height: '100%' }}>
-        <BrowserRouter>
+        {/* <BrowserRouter>
           <Routes>
             <Route path="/" element={<Signup />} />
             <Route path="/login" element={<Login />} />
@@ -51,7 +32,10 @@ function App() {
             <Route path="*" element={<ErrorPage />} />
           </Routes>
         </BrowserRouter>
-        <ToastContainer />
+        <ToastContainer /> */}
+        <Sidebar />
+        <Ticket />
+
       </div>
   )
 }
--- ReactJS/react-projects/src/assignment-2/class/Tickets/Ticket.jsx
+++ ReactJS/react-projects/src/assignment-2/class/Tickets/Ticket.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import Header from "../Header/Header";
 import Dashboard from "./Dashboard/Dashboard";
-import useTickets from '../../../assignment-5/hooks/useTickets';
 
 import avatar from'../../images/avatar.png'
 import customer1 from '../../images/customer-1.png';
